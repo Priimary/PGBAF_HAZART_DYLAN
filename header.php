999b4ea26363cec29c4cc27dc790d48f4523f1db
Priimary <dylan.hazart@gmail.com>
2021-05-15 22:07:29 +0200

    Ajout logo différents acteurs & illustration gbaf, mise à jour de tout l'html dans l'index, ajout de tout le php dans l'index & header, début de mise en forme en css.

--- header.php
+++ header.php
@@ -1,16 +1,27 @@
-<!-- insert the logo & the menu if the user is connected -->
-<div class="logo-gbaf-connected-container">
-	<img src="logo_gbaf.png" alt="logo gbaf">
-</div>
-<div class="nav-menu-header-container">
-	<nav class="nav-menu-header">
-		<li class="dropdown"><a id="nomprenomheader">Nom&Prénom</a>
-			<ul class="below">
-				<li><a href="lien-gestion-compte">Gestion du compte</a></li>
-				<li><a href="lien-deconnexion">Déconnexion</a></li>
-			</ul>
-		</li>
-	</nav>		
-</div>
-
-
+<?php
+    session_start();
+    if($_SESSION['loggedIn'] = true)
+        {
+           ?> 
+			<div class="logo-gbaf-connected-container">
+				<a href="index.php"><img src="img/logo_gbaf.png" alt="logo gbaf"></a>	
+			</div>
+			<div class="nav-menu-header-container">
+				<nav class="nav-menu-header">
+					<li class="dropdown"><a id="nomprenomheader"><?php echo $_SESSION['nom']," ", $_SESSION['prenom']; ?></a>
+						<ul class="submenu">
+							<li><a href="lien-gestion-compte">Gestion du compte</a></li>
+							<li><a href="lien-deconnexion">Déconnexion</a></li>
+						</ul>
+					</li>
+				</nav>		
+			</div>
+			<?php
+		}
+	else
+		{
+			?>
+			<a class="logo-gbaf-disconnected" href="index.php"><img class="logo-gbaf-disconnected" src="img/logo_gbaf.png" alt="logo gbaf"></a>
+			<?php
+		}
+			?>
--- index.php
+++ index.php
@@ -1,78 +1,103 @@
+<?php
+    session_start();
+    if($_SESSION['loggedIn'] != true)
+        {
+            header('Location: connexion.php');
+            exit;
+        }
+?>
 <!DOCTYPE html>
-<html>
+<html lang="fr">
     <head>
-        <meta charset="utf-8" />
+        <meta charset="utf-8"/>
     	<title>Extranet GBAF</title>
-    	<link rel="stylesheet" href="style.css" />
+    	<link rel="stylesheet" href="style.css"/>
+        <meta name="viewport" content="width=device-width"/>
     </head>
     <body>
-        <div class="index-corpse">
-    		<!-- insert header -->
+        <!-- bloc page entière index -->
+        <div id="index-container">
+    		<!-- EN-TETE -->
     		<header class="site-header-container">
-			<div class="site-header-content"><?php include 'header.php'; ?></div>
+                <div class="site-header-content"><?php include 'header.php'; ?></div>
 			</header>
-            <div class="index-content">
-                <!-- section for the gbaf entry -->
-                <section class="gbaf-section-container">
-    			<div class="gbaf-section-content">
-    				<div class="gbaf-title">
-    					<h1>Qu'est ce que GBAF et ce site ?</h1>
-    				</div>
-    				<div class="gbaf-introduction">
-    					<p>
-    						Le Groupement Banque-Assurance Français (GBAF) est une fédération représentant les 6 grands groupes français.<br />
-    						Même s'il existe une forte concurrence entre ces entités, elles vont toutes travailler de la même façon pour gérer près de 80 millions de comptes sur le territoire national.
-    						Le GBAF est le représentant de la profession bancaire et des assureurs sur tous les axes de la réglementation financière française. C'est aussi un interlocuteur priviligié des pouvoirs publics.
-    					</p>
-    				</div>
-    				<div class="gbaf-picture">
-    				<img src="image_gbaf" alt="img gbaf">
-    				</div>
-    			</div>
+            <!-- bloc contenu index entre en-tête et pied de page -->
+            <div id="index-content">
+
+                <!-- SECTION GBAF -->
+                <section id="gbaf-section-container">
+                    <!-- bloc contenu gbaf -->
+        			<article id="gbaf-section-content">
+                        <!--titre gbaf -->
+        				<h1>
+                            Qu'est ce que GBAF et ce site ?
+                        </h1>
+                        <!--description gbaf -->
+        				<p>
+        					Le Groupement Banque-Assurance Français (GBAF) est une fédération représentant les 6 grands groupes français.<br />
+        					Même s'il existe une forte concurrence entre ces entités, elles vont toutes travailler de la même façon pour gérer près de 80 millions de comptes sur le territoire national.
+        					Le GBAF est le représentant de la profession bancaire et des assureurs sur tous les axes de la réglementation financière française. C'est aussi un interlocuteur priviligié des pouvoirs publics.
+        				</p>
+                        <!-- bloc image gbaf -->
+        				<img src="img/illustration_gbaf.png" alt="logo des banques fondatrices gbaf">
+        			</article>
                 </section>
 
-                <!-- section for the actors entry -->
-                <section class="actors-section-container">
-    			<div class="actors-section-content">
-    				<div class="actors-title">
-    					<h2>Présentation des acteurs et partenaires</h2>
-    				</div>
-    				<div class="actors-introduction">
-    					<p>
-    						Plusieurs partenaires se sont engagés avec le GBAF pour proposer leurs services aux salariés, et ainsi augmenter leur confort.
-    					</p>
-    				</div>	
-    				<div class="actors-list">
-    					<div class="actors-entry">
-    						<div class="picture-actors-list">
-                                <img src="nom-img-recue">
-                            </div>
-                            <div class="title-content-actors-list">
-                                <div class="title-content-actors-list-inside">
-                                    <div class="title-actors-list">
+                <!-- SECTION ACTEURS -->
+                <section id="actors-section-container">
+                    <!-- bloc contenu acteur -->
+        			<article id="actors-section-content">
+                        <!-- bloc titre  presentation acteur -->
+        				<h2>
+                            Présentation des acteurs et partenaires
+                        </h2>
+                        <!-- bloc presentation acteurs -->
+        				<p>
+        					Plusieurs partenaires se sont engagés avec le GBAF pour proposer leurs services aux salariés, et ainsi augmenter leur confort.
+        				</p>
+                    </article>
+                    <!-- bloc conteneur liste acteurs -->
+                    <div id="actors-list-container">
+        				<?php
+                            try
+                            {
+                                $bdd = new PDO('mysql:host=localhost;dbname=oc_gbaf;charset=utf8', 'root', 'root');
+                            }
+                            catch(Exception $e)
+                            {
+                                die('Erreur : '.$e->getMessage());
+                            }
+                            $req = $bdd->query('SELECT id_acteur, acteur, description, logo FROM acteur');
+                            while ($donnees = $req->fetch())
+                            {
+                        ?>
+                                <!-- article pour chaque acteur -->
+        				        <article class="actors-entry">
+                                    <!-- image acteur liste -->
+                                    <img src="img/<?php echo htmlspecialchars($donnees['logo']); ?>">
+                                
+                                    <!-- bloc conteneur titre et description acteur liste -->
+                                    <div class="actors-entry-content">
                                         <h3>
-                                            titre acteurs
+                                            <?php echo htmlspecialchars($donnees['acteur']); ?>
                                         </h3>
-                                    </div>
-                                    <div class="content-actors-list">
                                         <p>
-                                            Contenu textuel 
+                                            <?php echo htmlspecialchars($donnees['description']); ?> 
                                         </p>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="link-actors-next-page-list">
-                                <a href="lien-récupération-données-acteurs">Lire la suite</a>
-                            </div>
-    					</div>
-    				</div>
-    			</div>	
-                </section>
+                                    <a href="actors.php?acteur=<?php echo $donnees['id_acteur']; ?>">Lire la suite</a>
+                                </article>
+                                <?php
+                                    }
+                                    $req->closeCursor();
+                                ?>
+        			</div>
+        		</section>
             </div>
-    		<!-- insert footer -->
+    		<!-- PIED DE PAGE -->
 			<footer class="site-footer-container">
-			<div class="site-footer-content"><?php include 'footer.php'; ?></div>
+                <div class="site-footer-content"><?php include 'footer.php'; ?></div>
 			</footer>
 		</div>
     </body>
-    </html>
+</html>
